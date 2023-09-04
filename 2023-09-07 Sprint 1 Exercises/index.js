130c40c1c0e8b7c3ec9c54ecacd7d2c99450cabb
Austin Shearin <austinmshearin@gmail.com>
2023-09-04 15:52:32 -0500

    Completed mastering basic arrow functions

--- 2023-09-07 Sprint 1 Exercises/index.js
+++ 2023-09-07 Sprint 1 Exercises/index.js
@@ -1,4 +1,5 @@
 // Understanding Basic Functions in JavaScript
+console.groupCollapsed("Understanding Basic Functions");
 
 // A no-parameter function displaying a greeting
 greeting = function() {
@@ -17,3 +18,22 @@ twoNumSum = function(num1, num2) {
     return num1 + num2;
 }
 console.log(`Sum: ${twoNumSum(2,3)}`);
+
+console.groupEnd();
+
+// Mastering Basic Arrow Functions in JavaScript
+console.groupCollapsed("Mastering Basic Arrow Functions");
+
+// A function that accepts two arguments and returns their product
+let twoNumProd = (num1, num2) => num1 * num2;
+console.log(twoNumProd(2, 3));
+
+// A function that takes in a string and returns it in uppercase
+let textUpper = text => text.toUpperCase();
+console.log(textUpper("Test text"));
+
+// A function with a single parameter, calculating the square of the provided number
+let numSquare = num => num * num;
+console.log(numSquare(4));
+
+console.groupEnd();
