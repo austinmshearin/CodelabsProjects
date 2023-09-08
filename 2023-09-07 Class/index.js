021bc5949968495d17290b971f2c1f9b24a529c1
Austin Shearin <austinmshearin@gmail.com>
2023-09-07 19:03:10 -0500

    Updates to exercise 2 with hoisted function within the main function

--- 2023-09-07 Class/index.js
+++ 2023-09-07 Class/index.js
@@ -1,23 +1,27 @@
 // Exercise #1
 console.groupCollapsed("Function Scope");
 function outerFunction() {
-  let outerVar = "I'm outside!";
+    let outerVar = "I'm outside!";
 
-  function innerFunction() {
-    let innerVar = "I'm inside!";
-    console.log(outerVar); // I'm outside!
-    console.log(innerVar); // I'm inside!
-  }
-  innerFunction();
+    function innerFunction() {
+        let innerVar = "I'm inside!";
+        console.log(outerVar); // I'm outside!
+        console.log(innerVar); // I'm inside!
+    }
+    innerFunction();
 }
 outerFunction();
 console.groupEnd();
 
 // Exercise #2
 console.groupCollapsed("Function Hoisting");
-mainFunction();
 function mainFunction() {
-  console.log("Testing function hoisting");
+    // Call the hoistedFunction here
+    hoistedFunction();
+    // Now, declare the hoistedFunction below
+    function hoistedFunction() {
+        console.log("Testing function hoisting");
+    }
 }
 mainFunction();
 console.groupEnd();
